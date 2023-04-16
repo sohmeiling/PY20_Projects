fdd976a50a8e57228a77a923d4388e5855825fe4
Soh Mei Ling <meigemini.91@gmail.com>
2023-04-16 14:38:59 +0800

    update brosnan

--- M2_memory_card_app/Brosnan_memory_card_app copy.py
+++ M2_memory_card_app/Brosnan_memory_card_app copy.py
@@ -1,10 +1,29 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QGroupBox, QButtonGroup, QRadioButton, QPushButton, QLabel, QVBoxLayout, QHBoxLayout)
+from PyQt5.QtWidgets import (QApplication, QWidget, QGroupBox, 
+                             QButtonGroup, QRadioButton, QPushButton, 
+                             QLabel, QVBoxLayout, QHBoxLayout)
+from random import shuffle, randint
 
 app = QApplication([])
 my_win = QWidget()
 my_win.setWindowTitle("Memory Card")
 my_win.resize(400,300)
+
+# Create a class for question
+class Question():
+    def __init__(self, questions, right_answer, wrong1, wrong2, wrong3):
+        self.questions = questions
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+#Questions
+questions_list = []
+questions_list.append(Question("What is the national language for Malaysia?", "Malay", "English", "Mandarin", "Tamil"))
+questions_list.append(Question('Official language of Brazil', 'Portuguese', 'English', 'Spanish', 'Brazilian'))
+questions_list.append(Question('Which color does not appear on the American flag?', 'Green', 'Red', 'White', 'Blue'))
+questions_list.append(Question('A traditional residence of the Yakut people', 'Urasa', 'Yurt', 'Igloo', 'Hut')) 
 
 def show_question():
     RadioGroupBox.show()
@@ -22,11 +41,49 @@ def show_result():
     AnsGroupBox.show()
     btn_ok.setText("Next question")
 
-def test():
-    if btn_ok.text() == "Answer":
-        show_result()
+def show_correct(res):
+    Ib_Result.setText(res)
+    show_result()
+
+def check_answer():
+    if answers[0].isChecked():
+        my_win.score +=1
+        show_correct('Correct!')
     else:
-        show_question()
+        if answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+            show_correct('Incorrect!')
+
+question_order = list(range(0, len(questions_list)))
+
+def click_OK():
+    if btn_ok.text() == "Answer":
+        if my_win.cur_question == 0:
+            shuffle(question_order)
+        check_answer()
+    else:
+        next_question()
+
+def ask (q:Question):
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_question.setText(q.questions) #question
+    Ib_Correct.setText(q.right_answer) #answer
+    show_question()
+
+def next_question():
+    my_win.total += 1
+    my_win.cur_question = my_win.cur_question + 1 
+    if my_win.cur_question >= len(questions_list):
+        shuffle(question_order)
+        my_win.cur_question = 0 
+        my_win.score = 0
+        my_win.total = 1
+    pick = question_order[my_win.cur_question]
+    q = questions_list[pick]
+    ask(q)
 
 lb_question = QLabel("When was the first play button given too?")
 btn_ok = QPushButton("Answer")
@@ -41,6 +98,8 @@ RadioGroup.addButton(rbtn_1)
 RadioGroup.addButton(rbtn_2)
 RadioGroup.addButton(rbtn_3)
 RadioGroup.addButton(rbtn_4)
+
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
 
 box_layout = QHBoxLayout()
 colum_left = QVBoxLayout()
@@ -84,6 +143,13 @@ layout_card.addStretch(1)
 layout_card.addLayout(layout_line3)
 
 my_win.setLayout(layout_card)
-btn_ok.clicked.connect(test)
+# ------------------ Main ------------------
+my_win.cur_question = -1  
+my_win.score = 0
+my_win.total = 0
+
+btn_ok.clicked.connect(click_OK)
+next_question()
+
 my_win.show()
 app.exec_()
